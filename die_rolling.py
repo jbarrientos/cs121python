8db2798c187043c4d15f96a185c7e3fb8ff62b9d
Jorge Barrientos <jorge.barrientos@gmail.com>
2023-02-14 22:49:33 -0800

    First commit

---
+++ die_rolling.py
@@ -0,0 +1,10 @@
+################################
+# Student: Jorge Barrientos
+# Instructor: Kaleab Gorfu
+# CS 121: Python for DS and ML
+# Date: 14/02/2023
+################################
+
+print(5 * (12.7 - 4) / 2)
+print(5 * 12.7 - 4 / 2)
+
