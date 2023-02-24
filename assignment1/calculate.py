c1e01e294cc6559a4481f3f7f8bf0c2df7b692e0
Jorge Barrientos <jorge.barrientos@gmail.com>
2023-02-24 12:29:32 -0800

    Assignment 1 - first version

---
+++ assignment1/calculate.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Date: Thu Feb 23 17:57:56 2023
+Instructor: Kaleab Gorfu
+Student: Jorge Barrientos
+CS 121: Python for DS and ML
+"""
+# Functions
+def calcTotal(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+def calcMin(numbers):
+    minNumber = 0
+    
+    if len(numbers) > 0:
+        minNumber = numbers[0]
+        
+    for num in numbers:
+        if num < minNumber:
+            minNumber = num
+    return minNumber
+
+def calcMax(numbers):
+    maxNumber = 0
+    
+    if len(numbers) > 0:
+        maxNumber = numbers[0]
+        
+    for num in numbers:
+        if num > maxNumber:
+            maxNumber = num
+            
+    return maxNumber
+
+def calcAvg(numbers):
+    return calcTotal(numbers) / len(numbers)
+        
+
+numbers = int(input('How many numbers do you want to calculate? (type 0 to exit) '))
+
+numsList = []
+numsRow = ''
+runningTotal = 0
+
+for i in range(numbers):
+    strNum = ''
+    cont = True
+    num = 0
+    while cont:        
+        strNum = input('Please enter the number '+str(i+1) + ': ')
+        num = float(strNum)        
+        if num == 0:
+            print('Please type a number different from zero.')
+            continue
+        else:
+            #
+            cont = False
+            numsList.append(num)
+            #
+            if i == numbers - 1:
+                numsRow += str(num)
+            else:
+                numsRow += str(num) + ','
+        
+        
+            
+if len(numsList) > 0:  
+    print('-------------------------------------------')   
+    print('Display each number in a row (Horizontally)')   
+    print('-------------------------------------------')   
+    print(numsRow)
+    print('--------------------------------------------------')   
+    print('Display each number in a row by itself(Vertically)')   
+    print('--------------------------------------------------')   
+    for num in numsList:
+        print(num)
+
+    print('-------------------------------')
+    print('RUNNING TOTAL: ' + str(calcTotal(numsList)))
+    print('MIN NUMBER: ' + str(calcMin(numsList)))
+    print('MAX NUMBER: ' + str(calcMax(numsList)))
+    print('AVERAGE: ' + str(calcAvg(numsList)))
+    print('-------------------------------')
+    
+
+   
+    
+
---
+++ assignment1/calculate_builtin_functions.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Date: Thu Feb 23 17:57:56 2023
+Instructor: Kaleab Gorfu
+Student: Jorge Barrientos
+CS 121: Python for DS and ML
+"""
+from statistics import mean       
+
+numbers = int(input('How many numbers do you want to calculate? (type 0 to exit) '))
+
+numsList = []
+numsRow = ''
+runningTotal = 0
+
+for i in range(numbers):
+    strNum = ''
+    cont = True
+    num = 0
+    while cont:        
+        strNum = input('Please enter the number '+str(i+1) + ': ')
+        num = float(strNum)        
+        if num == 0:
+            print('Please type a number different from zero.')
+            continue
+        else:
+            #
+            cont = False
+            numsList.append(num)
+            #
+            if i == numbers - 1:
+                numsRow += str(num)
+            else:
+                numsRow += str(num) + ','
+        
+        
+            
+if len(numsList) > 0:  
+    print('-------------------------------------------')   
+    print('Display each number in a row (Horizontally)')   
+    print('-------------------------------------------')   
+    print(numsRow)
+    print('--------------------------------------------------')   
+    print('Display each number in a row by itself(Vertically)')   
+    print('--------------------------------------------------')   
+    for num in numsList:
+        print(num)
+
+    print('-------------------------------')
+    print('RUNNING TOTAL: ' + str(sum(numsList)))
+    print('MIN NUMBER: ' + str(min(numsList)))
+    print('MAX NUMBER: ' + str(max(numsList)))
+    print('AVERAGE: ' + str(mean(numsList)))
+    print('-------------------------------')
+    
+
+   
+    
+
