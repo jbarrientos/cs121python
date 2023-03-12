bb5087ab44e264a99c7b6d31ef4d83997d13576b
Jorge Barrientos <jorge.barrientos@gmail.com>
2023-03-11 20:05:39 -0800

    Add tendency central functions: mean, median and mode

--- assignment1/calculate.py
+++ assignment1/calculate.py
@@ -3,6 +3,8 @@
 Date: Thu Feb 23 17:57:56 2023
 Instructor: Kaleab Gorfu
 Student: Jorge Barrientos
+Changes:
+    03/11/2023: Include central tendency functions: calcMean, calcMedian and calcMode
 CS 121: Python for DS and ML
 """
 # Functions
@@ -38,22 +40,71 @@ def calcMax(numbers):
 def calcAvg(numbers):
     return calcTotal(numbers) / len(numbers)
 
+def calcMean(numbers):
+    return calcTotal(numbers) / len(numbers)
+
+def calcMedian(numbers):
+    sortedList = numbers
+    sortedList.sort()
+    numbersLength = len(sortedList)
+    if numbersLength % 2 == 0:          
+        startIdx = int(numbersLength / 2) - 1
+        endIdx = int((numbersLength / 2))              
+        #print('startIdx',startIdx)
+        #print('endIdx',endIdx)
+        #print(sortedList)
+        medianVal = calcTotal([sortedList[startIdx],sortedList[endIdx]]) / 2.0
+    else:
+        midIndex = len(sortedList) // 2
+        medianVal = sortedList[midIndex]        
+        
+    return medianVal
+
+def calcMode(numbers):
+    modeDict = {}
+    for num in numbers:
+        strNum = str(num)
+        if strNum in modeDict:
+            modeDict[strNum] += 1
+        else:
+            modeDict[strNum] = 1
+            
+    #print('mode', modeDict.values())
+    #print(modeDict)
+    modeVal = 0.0
+    modeKey = ''
+    
+    for key in modeDict:
+        print(key)
+        if(modeDict[key] > modeVal):
+            modeVal = modeDict[key]
+            modeKey = key
+            
+            
+    return modeKey
+    
+
 print('+--------------------------------------------------------+')
 print('| Program to calculate numbers using self made functions | ') 
 print('+--------------------------------------------------------+')
         
-
+# Ask the user how many numbers wants to process
 numbers = int(input('How many numbers do you want to calculate? (type 0 to exit) '))
 
+# Variable to store the list of numbers that the user will enter.
 numsList = []
 numsRow = ''
-runningTotal = 0
 
+# Loop to ask the users the numbers to be processed, 
+# according to the quantity of numbers entered by the user 
 for i in range(numbers):
     strNum = ''
     cont = True
     num = 0
+    # While loop in cases where the user enter a zero value, 
+    # the program will ask for a different number.
     while cont:        
+        # Ask the number n
         strNum = input('Please enter the number '+str(i+1) + ': ')
         num = float(strNum)        
         if num == 0:
@@ -62,16 +113,18 @@ for i in range(numbers):
         else:
             #
             cont = False
+		# Append the entered number to the numbers list.
             numsList.append(num)
-            #
+            # Code to build the string that will have all the entered numbers in one line.
             if i == numbers - 1:
                 numsRow += str(num)
             else:
                 numsRow += str(num) + ','
         
         
-            
+# Evaluate if the list of numbers has at least one number.
 if len(numsList) > 0:  
+    # This code will be executed only if the listof number has at leat one element.
     print('-------------------------------------------')   
     print('Display each number in a row (Horizontally)')   
     print('-------------------------------------------')   
@@ -81,13 +134,17 @@ if len(numsList) > 0:
     print('--------------------------------------------------')   
     for num in numsList:
         print(num)
-
+    # Display values returned by the functions created above.
     print('-------------------------------')
     print('RUNNING TOTAL: ' + str(calcTotal(numsList)))
     print('MIN NUMBER: ' + str(calcMin(numsList)))
     print('MAX NUMBER: ' + str(calcMax(numsList)))
     print('AVERAGE: ' + str(calcAvg(numsList)))
+    print('MEAN: ' + str(calcMean(numsList)))
+    print('MEDIAN: ' + str(calcMedian(numsList)))
+    print('MODE: ' + str(calcMode(numsList)))
     print('-------------------------------')
+    
     
 
    
--- assignment1/calculate_builtin_functions.py
+++ assignment1/calculate_builtin_functions.py
@@ -4,24 +4,32 @@ Date: Thu Feb 23 17:57:56 2023
 Instructor: Kaleab Gorfu
 Student: Jorge Barrientos
 CS 121: Python for DS and ML
+Changes:
+    03/11/2023: Include builin central tendency functions: mean, median and mode
 """
-from statistics import mean       
+from statistics import mean, mode, median       
 
-print('+-------------------------------------------------------------+')
-print('| Program to calculate numbers with Python built in functions | ') 
-print('+-------------------------------------------------------------+')
-
+print('+--------------------------------------------------------+')
+print('| Program to calculate numbers using self made functions | ') 
+print('+--------------------------------------------------------+')
+        
+# Ask the user how many numbers wants to process
 numbers = int(input('How many numbers do you want to calculate? (type 0 to exit) '))
 
+# Variable to store the list of numbers that the user will enter.
 numsList = []
 numsRow = ''
-runningTotal = 0
 
+# Loop to ask the users the numbers to be processed, 
+# according to the quantity of numbers entered by the user 
 for i in range(numbers):
     strNum = ''
     cont = True
     num = 0
+    # While loop in cases where the user enter a zero value, 
+    # the program will ask for a different number.
     while cont:        
+        # Ask the number n
         strNum = input('Please enter the number '+str(i+1) + ': ')
         num = float(strNum)        
         if num == 0:
@@ -30,16 +38,18 @@ for i in range(numbers):
         else:
             #
             cont = False
+		# Append the entered number to the numbers list.
             numsList.append(num)
-            #
+            # Code to build the string that will have all the entered numbers in one line.
             if i == numbers - 1:
                 numsRow += str(num)
             else:
                 numsRow += str(num) + ','
         
         
-            
+# Evaluate if the list of numbers has at least one number.
 if len(numsList) > 0:  
+    # This code will be executed only if the listof number has at leat one element.
     print('-------------------------------------------')   
     print('Display each number in a row (Horizontally)')   
     print('-------------------------------------------')   
@@ -49,12 +59,16 @@ if len(numsList) > 0:
     print('--------------------------------------------------')   
     for num in numsList:
         print(num)
+    # Display values returned by the functions created above.
 
     print('-------------------------------')
     print('RUNNING TOTAL: ' + str(sum(numsList)))
     print('MIN NUMBER: ' + str(min(numsList)))
     print('MAX NUMBER: ' + str(max(numsList)))
     print('AVERAGE: ' + str(mean(numsList)))
+    print('MEAN: ' + str(mean(numsList)))
+    print('MEDIAN: ' + str(median(numsList)))
+    print('MODE: ' + str(mode(numsList)))
     print('-------------------------------')
     
 
